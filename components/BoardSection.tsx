047b451fef6e8e1ede64767448e959ed1c3509a6
judygab <ivditigabeskiria@gmail.com>
2021-11-27 23:09:56 -0500

    added create a task form

--- components/BoardSection.tsx
+++ components/BoardSection.tsx
@@ -1,36 +1,88 @@
-import React from 'react';
-import { Col, Button, Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Col, Button, Card, Form } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { Modal } from 'react-bootstrap';
+import { gql, useMutation } from '@apollo/client';
 
+const CreateTaskMutation = gql`
+  mutation CreateTask($id: String, $title: String!, $description: String!) {
+    createTask(id: $id, title: $title, description: $description) {
+      id
+      title
+      description
+    }
+  }
+`
 
 interface BoardSectionProps {
   title: String
 }
 
 const BoardSection: React.FC<BoardSectionProps> = ({ title }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [taskTitle, setTaskTitle] = useState('');
+  const [taskDescription, setTaskDescription] = useState('');
+
+  const [createTask, { data, loading, error }] = useMutation(CreateTaskMutation);
+
+  const handleClose = () => setShowModal(false)
+  const handleShow = () => setShowModal(true)
+  const handleTaskCreate = (e) => {
+    e.preventDefault();
+    createTask({ variables: { title: taskTitle, description: taskDescription  } });
+    handleClose();
+  }
+
   return (
-    <Col md={3} className="d-flex flex-column p-2">
-      <div className="board-section-header d-flex flex-row align-items-center">
-        <h3 className="me-auto">{title}</h3>
-        <FontAwesomeIcon icon={faPlus} style={{'color': '#6f7782'}}/>
-      </div>
-      {/*<Card>
-        <Card.Body>
-          this is some ticket
-        </Card.Body>
-      </Card>
-      <Button className="add-wrapper">
-        <FontAwesomeIcon icon={faPlus} style={{'padding': '2px'}}/>
-        Add task
-      </Button>*/}
-      <div className="is-empty d-flex flex-column">
+    <>
+      <Col md={3} className="d-flex flex-column p-2">
+        <div className="board-section-header d-flex flex-row align-items-center">
+          <h3 className="me-auto">{title}</h3>
+          <FontAwesomeIcon icon={faPlus} style={{'color': '#6f7782'}}/>
+        </div>
+        <Card className="ticket-container">
+          <Card.Body>
+            this is some ticket
+          </Card.Body>
+        </Card>
         <Button className="add-wrapper">
           <FontAwesomeIcon icon={faPlus} style={{'padding': '2px'}}/>
           Add task
         </Button>
-      </div>
-    </Col>
+        <div className="is-empty d-flex flex-column">
+          <Button className="add-wrapper" onClick={handleShow}>
+            <FontAwesomeIcon icon={faPlus} style={{'padding': '2px'}}/>
+            Add task
+          </Button>
+        </div>
+      </Col>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create a Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleTaskCreate}>
+            <Form.Group className="mb-3">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)}></Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Description</Form.Label>
+              <Form.Control as="textarea" style={{ height: '100px'}} value={taskDescription} onChange={(e) => setTaskDescription(e.target.value)}></Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Assign To</Form.Label>
+              <Form.Select aria-label="Assign To">
+              </Form.Select>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 }
 
